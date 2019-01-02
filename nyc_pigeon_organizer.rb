a0af78f28191eba3fb82fb8c467a511913ef11d0
Kyle Houghton <kylebenz@hotmail.com>
2019-01-02 01:41:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,41 +1,7 @@
 
-require 'pry'
-
-data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
 def bird_list(data)
   data.collect {|key, value| value.values}.flatten.uniq
 end
-
-=begin
-def properties_of(data, bird)
-  return_hash = {bird => {}}
-  data.each do |key, value| 
-    return_hash[bird][key] = []
-    value.each do |subkey, subkey_value|
-      return_hash[bird][key] << subkey if subkey_value.include?(bird)
-    end
-  end
-  return_hash
-end
-=end
 
 def properties_of(data, bird)
   return_hash = {}
@@ -55,8 +21,3 @@ def nyc_pigeon_organizer(data)
   end
   return_hash
 end
-
-#bird = "Theo"
-#binding.pry
-#properties_of(data, bird)
-
